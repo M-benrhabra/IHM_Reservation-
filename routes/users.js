cf378eb6f1e3318897fcb035499f0a0345479eac
m-benrhabra <meriem.benrhabra1@gmail.com>
2021-02-19 11:40:56 +0100

    Modification

--- routes/users.js
+++ routes/users.js
@@ -1,56 +1,38 @@
 const express = require('express');
 const router = express.Router();
 const sequelizeDb = require('../config/database');
-const Offer = require('../models/User');
+const User = require('../models/User');
 const Sequelize = require('sequelize');
 const Op = Sequelize.Op;
 
-
-// router.post('/add', (req, res) => {
-//     let { FName, LName, Contact, Email, Password } = req.body;
-//     let errors = [];
-  
-//     // Validate Fields
-//     if(!FName) {
-//       errors.push({ text: 'Please Add Your First Name' });
-//     }
-//     if(!LName) {
-//       errors.push({ text: 'Please Add Your Last Name' });
-//     }
-//     if(!Contact) {
-//       errors.push({ text: 'Please add a Number Phone' });
-//     }
-//     if(!Email) {
-//       errors.push({ text: 'Please add a contact email' });
-//     }
-//      // Check for errors
-//   if(errors.length > 0) {
-//     res.render('confirm', {
-//       errors,
-//       title, 
-//       technologies, 
-//       budget, 
-//       description, 
-//       contact_email
-//     });
-//   } else {
-//     if(!budget) {
-//       budget = 'Unknown';
-//     } else {
-//       budget = `$${budget}`;
-//     }
-
-//     // Insert into table
-//     User.create({
-//         firstName,
-//         lastName,
-//         contact,
-//         email,
-//         password
-//     })
-//       .then(gig => res.redirect('/users'))
-//       .catch(err => res.render('error', {error:err.message}))
-//   }
+// Get Ticket 
+// router.get('/', (req, res) =>
+// User.findAll()
+//     .then(users => res.render('users', {
+//         users
+//     }))
+//     .catch(err => res.render('error', { error: err })
+//     ));
+router.get('/register', (req, res) => {
+    res.render('views');
+});
+// router.get('/', (req, res) => {
+//     res.render('register');
 // });
+
+router.post('/add', (req, res) => {
+    let { FName, LName, Contact, Email, Password } = req.body;
+
+    // Insert into table
+    User.create({
+        firstName: FName,
+        lastName: LName,
+        contact: Contact,
+        email: Email,
+        password: Password
+    })
+        .then(user => res.redirect('register'))
+        .catch(err => res.render('error', { error: err.message }))
+});
 
 module.exports = router; 
